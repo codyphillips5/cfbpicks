f39cb9e5be15f1c330e5a09dd013b126c572d656
codyphillips5 <codyphill5@gmail.com>
2019-08-26 01:05:57 -0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -3,17 +3,9 @@ var game = {
   spread : "-",
   pts : "-",
 };
-function showDiv(divId, element){
-	document.getElementById(divId).style.display = element.value != "" ? 'block' : 'none';
-	game.team = element.value;
-}
-function setTeam(element) {
-	game.team = element.value;
-}
-function myNewFunction(sel) {
-	var pts = sel;
-	document.getElementById(pts).innerHTML = game.team;
-}
+
+var choices = [{team: "", game: "", spread : "-", pts : "50"}, {team: "", game: "", spread : "-", pts : "40"}, {team: "", game: "", spread : "-", pts : "30"}, {team: "", game: "", spread : "", pts : "20"}, {team: "", game: "", spread : "", pts : "10"}];
+
 $.getJSON("https://codyphillips5.github.io/cfbpicks/week1.json", function(json) {
 	for (var key in json) {
 		for (var i = 0; i < json[key].length; i++) {
@@ -44,19 +36,54 @@ $.getJSON("https://codyphillips5.github.io/cfbpicks/week1.json", function(json) 
 				badge.className = 'games-layout';		
 			}
 			var header = '<span class=\'header\'><h3>' + awayTeam + ' vs ' + homeTeam + ' (' + homeSide + spread + ') </h3>';
-			var select = '<select class=\'teamlist\' id=\'game\' onchange=\"showDiv(\'point_totals_game_' + gameId + '\', this);\"><option value = \"\"> -- Select Team -- </option><option value=\"' + awayTeamVal + '\">' + awayTeam + ' ' + awaySide + spread + '</option><option value=\"' + homeTeamVal + '\">' + homeTeam + ' ' + homeSide + spread + '</option></select>';
+			var select = '<select class=\'teamlist\' id=\'game' + gameId + '\' onchange=\"showPointTotals(\'point_totals_game_' + gameId + '\', this);\"><option value = \"\"> -- Select Team -- </option><option value=\"' + awayTeamVal + '\">' + awayTeam + ' ' + awaySide + spread + '</option><option value=\"' + homeTeamVal + '\">' + homeTeam + ' ' + homeSide + spread + '</option></select>';
 			var display = '<div id=\"point_totals_game_' + gameId + '\" style=\"display:none;\">';
 			var numbers = [5, 4, 3, 2, 1];
 			var radios = [];
 			for (var j = 0; j < numbers.length; j++) {
-				var radio = '<label><input type=\"radio\" onchange=\"myNewFunction(this.value);\" name=\"pick_worth\" value=' + numbers[j] + '0>' + numbers[j] + '0</label>'
+				if (required) {
+					var radio = '<label><input type=\"radio\" onchange=\"assignPointsByTeam(this.value, ' + gameId +');\" name=\"pick_worth\" value=' + numbers[j] + '0 required>' + numbers[j] + '0</label>'
+				} 
+				else {
+					var radio = '<label><input type=\"radio\" onchange=\"assignPointsByTeam(this.value, ' + gameId +');\" name=\"pick_worth\" value=' + numbers[j] + '0>' + numbers[j] + '0</label>'
+				}
 				radios.push(radio);
 			}
-
-
 			badge.innerHTML = '<form>' + header + select + '<br>'+ display + radios.join(' ') + '</form>';		
 			document.getElementById(key).appendChild(badge);
 		}
 	}
-	console.log(json); // this will show the info it in firebug console
 });
+
+function assignPointsByTeam(pts, id) {
+	var pick = document.getElementById("game" + id);
+	var userPick = pick.options[pick.selectedIndex].value;
+	game.team = userPick;
+	document.getElementById(pts).innerHTML = game.team;
+	for (i = 0; i < choices.length; i++) {
+		// only allow a team to be chosen once
+		if(game.team == choices[i].team) {
+			choices[i].team = "";
+			document.getElementById(choices[i].pts).innerHTML = "";
+		}
+		if(pts == choices[i].pts) {
+			choices[i].team = userPick;
+			choices[i].game = id;
+		}
+		/*if(choices[i].pts != "") {
+			console.log("clearing " + choices[i].game);
+			document.getElementById("point_totals_game_" + choices[i].game).getElementsByTagName("input")[0].checked = false;
+		}*/
+	}
+	document.getElementById(pts).innerHTML = game.team;
+	console.log(choices);
+}
+
+function showPointTotals(divId, element){
+	document.getElementById(divId).style.display = element.value != "" ? 'block' : 'none';
+	game.team = element.value;
+}
+
+function setTeam(element) {
+	game.team = element.value;
+}

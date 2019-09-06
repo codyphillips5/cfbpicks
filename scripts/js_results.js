101979249eeb5e4511e18be2785fa6ab0bdeb57c
codyphillips5 <codyphill5@gmail.com>
2019-09-06 14:58:17 -0400

    Add files via upload

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -55,7 +55,7 @@ $.when(getResults, getTeams).then(function(){
 });
 
 $.when(getPicks, getResults, getTeams, getUsers).then(function(){
-	var tableStart = `<table class="table table-hover"><thead><tr><th scope="col">Name</th><th scope="col">50</th><th scope="col">40</th><th scope="col">30</th><th scope="col">20</th><th scope="col">10</th><th scope="col">Total</th></tr></thead><tbody>`;
+	var tableStart = `<table class="table table-hover" id="results"><thead><tr><th scope="col">Name</th><th scope="col">50</th><th scope="col">40</th><th scope="col">30</th><th scope="col">20</th><th scope="col">10</th><th scope="col">Total</th></tr></thead><tbody>`;
 
 	for (var key in picksList) {
 		for (var i = 0; i < picksList[key].length; i++) {
@@ -91,4 +91,60 @@ $.when(getPicks, getResults, getTeams, getUsers).then(function(){
 	}
 	var tableEnd = `</tbody></table>`;	
 	document.getElementById("standings").innerHTML = tableStart + tableUser + tableEnd;
+	sortTable(6);
 });
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("results");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "desc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n-1];
+      y = rows[i + 1].getElementsByTagName("TD")[n-1];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (Number(x.innerHTML) > Number(y.innerHTML)) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (Number(x.innerHTML) < Number(y.innerHTML)) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++;      
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}

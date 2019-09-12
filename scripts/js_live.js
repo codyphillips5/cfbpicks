26a136e6d549ff01d151a67bfcdfa0208b1e4bae
codyphillips5 <codyphill5@gmail.com>
2019-09-12 01:08:18 -0400

    Add files via upload

--- scripts/js_live.js
+++ scripts/js_live.js
@@ -3,30 +3,30 @@ var lastName = "";
 var coversNum = [];
 var coversTeam = [];
 
-var picksList, teamsList, resultsList, usersList;
+var standings, standings, resultsList, usersList;
 
-var getPicks = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week2_picks.json", function(json){
-	picksList = json;
+var getPicks = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week3_picks.json", function(json){
+	standings = json;
 });
 
-var getTeams = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/teams.json", function(json){
-	teamsList = json;
+var getStandings = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/teams.json", function(json){
+	standings = json;
 });
 
 var getUsers= $.getJSON("https://codyphillips5.github.io/cfbpicks/json/users.json", function(json){
     usersList = json;
 });
 
-$.when(getPicks, getTeams, getUsers).then(function(){
+$.when(getPicks, getStandings, getUsers).then(function(){
 	var tableStart = `<table class="table table-hover" id="results"><thead><tr><th scope="col">Name</th><th scope="col">50</th><th scope="col">40</th><th scope="col">30</th><th scope="col">20</th><th scope="col">10</th></tr></thead><tbody>`;
 
-	for (var key in picksList) {
-		for (var i = 0; i < picksList[key].length; i++) {
+	for (var key in standings) {
+		for (var i = 0; i < standings[key].length; i++) {
 			// set starters
 			var pointTotal = 0;
 			var isCorrect;
 
-			var user = picksList[key][i].userId;
+			var user = standings[key][i].userId;
 			// get user info
 			for (var k in usersList) {
 				for (var j = 0; j < usersList[k].length; j++) {
@@ -39,7 +39,7 @@ $.when(getPicks, getTeams, getUsers).then(function(){
 			var tableUser = tableUser + `<tr><th>${firstName + " " + lastName}</th>`;
 			// check user picks against results
 			for (var pointTotals = 50; pointTotals >= 10; pointTotals = pointTotals-10) {
-				tableUser = tableUser + `<td>${picksList[key][i][pointTotals]}</td>`;
+				tableUser = tableUser + `<td>${standings[key][i][pointTotals]}</td>`;
 			}
 		}
 	}

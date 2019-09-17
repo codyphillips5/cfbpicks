1656abc7014f21a0f95f3ee177e08ff2b78b2a2b
codyphillips5 <codyphill5@gmail.com>
2019-09-17 00:06:56 -0400

    Add files via upload

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -4,81 +4,93 @@ var coversNum = [];
 var coversTeam = [];
 
 var picksList, teamsList, resultsList, usersList;
+//var badge = document.createElement('div');
+//badge.className = 'standings';
+//var select = `<select class='form-control' id='results_by_week' onchange="getResultsByWeek(this.value);"><option value =''> Select Week </option><option value ='1'> Week 1 </option><option value ='2'> Week 2 </option></select>`;
+//badge.innerHTML = '<form>' + select + '</form>';		
+//document.getElementById("weeks").appendChild(badge);
 
-var getPicks = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week2_picks.json", function(json){
-	picksList = json;
-});
+getResultsByWeek(3);
 
-var getResults = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week2_results.json", function(json){
-	resultsList = json;
-	// get results
-	for (var result in resultsList) {
-		for (var r = 0; r < resultsList[result].length; r++) {
-			coversNum.push(resultsList[result][r].cover);
-		}
-	}
-});
-
-var getTeams = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/teams.json", function(json){
-	teamsList = json;
-});
-
-var getUsers= $.getJSON("https://codyphillips5.github.io/cfbpicks/json/users.json", function(json){
-    usersList = json;
-});
-
-$.when(getResults, getTeams).then(function(){
-	for (var cov = 0; cov < coversNum.length; cov++) {
-		for (var team in teamsList) {
-			for (var t = 0; t < teamsList[team].length; t++) {
-				if (coversNum[cov] == teamsList[team][t].teamId) {
-					coversTeam.push(teamsList[team][t].teamValue);
-				}
+function getResultsByWeek(week) {
+	console.log(week);
+	var getPicks = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week"+ week +"_picks.json", function(json){
+		picksList = json;
+	});
+	
+	var getResults = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week" + week + "_results.json", function(json){
+		resultsList = json;
+		// get results
+		for (var result in resultsList) {
+			for (var r = 0; r < resultsList[result].length; r++) {
+				coversNum.push(resultsList[result][r].cover);
+				console.log(resultsList[result][r].cover);
 			}
 		}
-	}
-});
-
-$.when(getPicks, getResults, getTeams, getUsers).then(function(){
-	var tableStart = `<table class="table table-hover" id="results"><thead><tr><th scope="col">Name</th><th scope="col">50</th><th scope="col">40</th><th scope="col">30</th><th scope="col">20</th><th scope="col">10</th><th scope="col">Total</th></tr></thead><tbody>`;
-
-	for (var key in picksList) {
-		for (var i = 0; i < picksList[key].length; i++) {
-			// set starters
-			var pointTotal = 0;
-			var isCorrect;
-
-			var user = picksList[key][i].userId;
-			// get user info
-			for (var k in usersList) {
-				for (var j = 0; j < usersList[k].length; j++) {
-						if (user == usersList[k][j].userId) {
-							firstName = usersList[k][j].FirstName;
-							lastName = usersList[k][j].LastName;
+	});
+	
+	var getTeams = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/teams.json", function(json){
+		teamsList = json;
+	});
+	
+	var getUsers= $.getJSON("https://codyphillips5.github.io/cfbpicks/json/users.json", function(json){
+		usersList = json;
+	});
+	console.log(picksList);
+	console.log(resultsList);
+	$.when(getResults, getTeams).then(function(){
+		for (var cov = 0; cov < coversNum.length; cov++) {
+			for (var team in teamsList) {
+				for (var t = 0; t < teamsList[team].length; t++) {
+					if (coversNum[cov] == teamsList[team][t].teamId) {
+						coversTeam.push(teamsList[team][t].teamValue);
 					}
 				}
 			}
-			var tableUser = tableUser + `<tr><th>${firstName + " " + lastName}</th>`;
-			// check user picks against results
-			for (var pointTotals = 50; pointTotals >= 10; pointTotals = pointTotals-10) {
-				if(coversTeam.includes(picksList[key][i][pointTotals])) {
-					isCorrect = "success";
-					pointTotal = pointTotal + pointTotals;
-				}
-				else {
-					isCorrect = "danger";				
-				}
-				tableUser = tableUser + `<td class="${isCorrect}">${picksList[key][i][pointTotals]}</td>`;
-			}
-			//calculate score			
-			tableUser = tableUser + `<td>${pointTotal}</td></tr>`;
 		}
-	}
-	tableUser = tableUser.replace("undefined","");
-	var tableEnd = `</tbody></table>`;	
-	document.getElementById("standings").innerHTML = tableStart + tableUser + tableEnd;
-	sortTable(6);
-});
+	});
+	
+	$.when(getPicks, getResults, getTeams, getUsers).then(function(){
+		var tableStart = `<table class="table table-hover" id="results"><thead><tr><th scope="col">Name</th><th scope="col">50</th><th scope="col">40</th><th scope="col">30</th><th scope="col">20</th><th scope="col">10</th><th scope="col">Total</th></tr></thead><tbody>`;
+	
+		for (var key in picksList) {
+			for (var i = 0; i < picksList[key].length; i++) {
+				// set starters
+				var pointTotal = 0;
+				var isCorrect;
+	
+				var user = picksList[key][i].userId;
+				// get user info
+				for (var k in usersList) {
+					for (var j = 0; j < usersList[k].length; j++) {
+							if (user == usersList[k][j].userId) {
+								firstName = usersList[k][j].FirstName;
+								lastName = usersList[k][j].LastName;
+						}
+					}
+				}
+				var tableUser = tableUser + `<tr><th>${firstName + " " + lastName}</th>`;
+				// check user picks against results
+				for (var pointTotals = 50; pointTotals >= 10; pointTotals = pointTotals-10) {
+					if(coversTeam.includes(picksList[key][i][pointTotals])) {
+						isCorrect = "success";
+						pointTotal = pointTotal + pointTotals;
+					}
+					else {
+						isCorrect = "danger";				
+					}
+					tableUser = tableUser + `<td class="${isCorrect}">${picksList[key][i][pointTotals]}</td>`;
+				}
+				//calculate score			
+				tableUser = tableUser + `<td>${pointTotal}</td></tr>`;
+			}
+		}
+		tableUser = tableUser.replace("undefined","");
+		var tableEnd = `</tbody></table>`;	
+		document.getElementById("standings").innerHTML = tableStart + tableUser + tableEnd;
+		sortTable(6);
+	});
+}
 
 function sortTable(n) {
   var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;

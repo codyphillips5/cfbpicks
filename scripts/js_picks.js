fe29c13d26246c42ab10aa203f0fea823acd65ca
codyphillips5 <codyphill5@gmail.com>
2019-09-30 20:40:14 -0400

    Add files via upload

--- scripts/js_picks.js
+++ scripts/js_picks.js
@@ -30,7 +30,7 @@ var game = {
   
   var xFile, yFile;
   
-  var requestX = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week5.json", function(json){
+  var requestX = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week6.json", function(json){
 	  xFile = json;
   });
   

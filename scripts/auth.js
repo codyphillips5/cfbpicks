5251c2447f899ce0f78c23935c3e4502b0061bf4
codyphillips5 <codyphill5@gmail.com>
2019-10-10 00:49:45 -0400

    week7

--- scripts/auth.js
+++ scripts/auth.js
@@ -22,7 +22,7 @@ if(createForm) {
     createForm.addEventListener('submit', (e) => {
         e.preventDefault();
     
-        db.collection('week6').add({
+        db.collection('week7').add({
             user: auth.currentUser.email,
             50: document.getElementById('50').value,
             40: document.getElementById('40').value,

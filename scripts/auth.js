efc745b66b7d32d2161e38eb3facb187aefa4918
codyphillips5 <codyphill5@gmail.com>
2019-11-10 02:52:39 -0500

    Add files via upload

--- scripts/auth.js
+++ scripts/auth.js
@@ -22,19 +22,15 @@ if(createForm) {
     createForm.addEventListener('submit', (e) => {
         e.preventDefault();
     
-        db.collection('week11').add({
+        db.collection('sunday').add({
             user: auth.currentUser.email,
-            50: document.getElementById('50').value,
-            40: document.getElementById('40').value,
-            30: document.getElementById('30').value,
-            20: document.getElementById('20').value,
-            10: document.getElementById('10').value
+            game10: document.getElementById('seasongame10').value
         }).then(() => {
             // close the modal and reset form
             //const modal = document.querySelector('#modal-create');
             //M.Modal.getInstance(modal).close();
             createForm.reset();
-            createForm.querySelector('.response').innerHTML = `<br><div class="alert alert-success" role="alert">Success! Your picks have been saved. At this time, picks may only be submitted ONCE. Please reach out to Kevin with questions or any pick changes.</div>`;
+            createForm.querySelector('.response').innerHTML = `<br><div class="alert alert-success" role="alert">Success! Your picks have been saved. At this time, picks may only be submitted ONCE. Please reach out to Cody with questions or any pick changes.</div>`;
         }).catch(err => {
             console.log(err.message)
             //signupForm.querySelector('.response').innerHTML = `<br><div class="alert alert-danger" role="alert">${err.message}</div>`;

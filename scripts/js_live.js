5aa3d4bc0c32f3c44faa2377850270aacda1f2dd
codyphillips5 <codyphill5@gmail.com>
2020-10-03 12:15:05 -0400

    marlin

--- scripts/js_live.js
+++ scripts/js_live.js
@@ -5,7 +5,7 @@ var coversTeam = [];
 
 var standings, teams, resultsList, usersList;
 
-var getPicks = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week3_picks.json", function(json){
+var getPicks = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week4_picks.json", function(json){
 	standings = json;
 });
 

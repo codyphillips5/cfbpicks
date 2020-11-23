661aefd30470f25c7ec9550c27083a2f6dcfecde
codyphillips5 <codyphill5@gmail.com>
2020-11-22 22:38:55 -0500

    dobby

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -7,11 +7,11 @@ var picksList, teamsList, usersList;
 var resultsList = [];
 var badge = document.createElement('div');
 badge.className = 'standings';
-var select = `<select class='form-control' id='results_by_week' onchange="getResultsByWeek(this.value);"><option value ='10'> Week 10 </option><option value ='9'> Week 9 </option><option value ='8'> Week 8 </option><option value ='7'> Week 7 </option><option value ='6'> Week 6 </option><option value ='5'> Week 5 </option><option value ='4'> Week 4 </option><option value ='3'> Week 3 </option><option value ='2'> Week 2 </option><option value ='1'> Week 1 </option></select>`;
+var select = `<select class='form-control' id='results_by_week' onchange="getResultsByWeek(this.value);"><option value ='11'> Week 11 </option><option value ='10'> Week 10 </option><option value ='9'> Week 9 </option><option value ='8'> Week 8 </option><option value ='7'> Week 7 </option><option value ='6'> Week 6 </option><option value ='5'> Week 5 </option><option value ='4'> Week 4 </option><option value ='3'> Week 3 </option><option value ='2'> Week 2 </option><option value ='1'> Week 1 </option></select>`;
 badge.innerHTML = '<form>' + select + '</form>';		
 document.getElementById("weeks").appendChild(badge);
 
-getResultsByWeek(10);
+getResultsByWeek(11);
 
 function getResultsByWeek(week) {
 	console.log(week);		

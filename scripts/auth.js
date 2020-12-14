1572c19257fb2c3861b8909f6e972a90c5001d3c
codyphillips5 <codyphill5@gmail.com>
2020-12-13 19:23:53 -0500

    hb

--- scripts/auth.js
+++ scripts/auth.js
@@ -30,7 +30,7 @@ if(createForm) {
     createForm.addEventListener('submit', (e) => {
         e.preventDefault();
     
-        db.collection('week14').doc(auth.currentUser.email).set({
+        db.collection('week15').doc(auth.currentUser.email).set({
             user: auth.currentUser.email,
             10: document.getElementById('10').value,
             20: document.getElementById('20').value,

6da6fe29d63f53685921bfc3019b9d3d195a0b4b
codyphillips5 <codyphill5@gmail.com>
2021-09-21 17:51:24 -0400

    marlin

--- scripts/js_results.js
+++ scripts/js_results.js
@@ -5,7 +5,7 @@ var coversNum = [];
 var coversTeam = [];
 var userPickTeams = [];
 var coversArr = [""];
-var weekNum = 3;
+var weekNum = 4;
 var isCorrect;
 var isMe;
 var date1 = new Date();
@@ -20,7 +20,7 @@ var resultsList = [];
 var badge = document.createElement('div');
 badge.className = 'results';
 var select = `<select class='form-control form-select' id='results_by_week' onchange="getResultsByWeek(this.value);">
-<option value ='3'> Week 3 </option><option value ='2'> Week 2 </option><option value ='1'> Week 1 </option>
+<option value ='4'> Week 4 </option><option value ='3'> Week 3 </option><option value ='2'> Week 2 </option><option value ='1'> Week 1 </option>
 </select>`;
 badge.innerHTML = '<form>' + select + '</form>';		
 document.getElementById("weeks").appendChild(badge);

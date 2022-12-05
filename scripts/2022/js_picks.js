2b8eb8bba0b2a733d2f145251f9586a4a3e14034
codyphillips5 <codyphill5@gmail.com>
2022-12-05 17:20:49 -0500

    fixer

--- scripts/2022/js_picks.js
+++ scripts/2022/js_picks.js
@@ -3,230 +3,279 @@ var game = {
 	spread : "",
 	pts : "",
 	game : "" 
-};
-
-attempt = {
+  };
+  var attempt = {
 	  thisTeam : "",
 	  thisMascot : "",
 	  thisTeamAbb : "",
 	  thisTeamImg : ""
-};
-
-const arrayActive = []; 
-  
-var gameId = "";
-var home = "";
-var homeTeam = "";
-var homeTeamVal = "";
-var homeTeamMascot = "";
-var homeTeamImage = "";
-var away = "";
-var awayTeam = "";
-var awayTeamVal = "";
-var spread = "";
-var fav ="";
-var homeSide = "";
-var awaySide = "";
-var required = "";
-var gog = "";
-var coversTeam = [];
-var overtime = "";
-var numOT = "";
+  };
   
 // week of year, first game
-var week = 5;
+var week = 14;
 if (week === 0) {
-	document.getElementById("week-title").innerHTML = `<h2>🏀 March Madness</h2>`;
-	document.getElementById("games-section").innerHTML = `<a href="totals.html">Sabermetrics</a>`;
+	document.getElementById("week-title").innerHTML = `<h2>Week 0 Test</h2>`;
 }
-else if (week === 3) {
-	document.getElementById("week-title").innerHTML = `<h2>🦃 Feast Week 🦃</h2>`;
-}
-else if (week === 7) {
-	document.getElementById("week-title").innerHTML = `<h2>🎅 Week 7 🎄</h2>`;
+else if (week === 14) {
+	document.getElementById("week-title").innerHTML = `<h2>Enjoy the Bowls!</h2>`;
 }
 else {
 	document.getElementById("week-title").innerHTML = `<h2>Week ${week}</h2>`;
 }
-
-var xFile, yFile;
-
-var requestX = $.getJSON("https://codyphillips5.github.io/cbbpicks/json/games/week" + week +".json", function(json){
-	xFile = json;
-});
-
-var requestY = $.getJSON("https://codyphillips5.github.io/cbbpicks/json/teams.json", function(json){
-	yFile = json;
-});
-
-var date1 = new Date();
-
-$.when(requestX, requestY).then(function(){
-	console.log(firebase.auth().currentUser);
-	for (var key in xFile) {
-		for (var i = 0; i < xFile[key].length; i++) {
-			var gameId = xFile[key][i].gameId;
-			// set home team values
-			var home = xFile[key][i].homeTeam;
-			for (var k in yFile) {
-				for (var j = 0; j < yFile[k].length; j++) {
-					if(home == yFile[k][j].teamValue) {
-						homeTeam = yFile[k][j].team;
-						homeTeamVal = yFile[k][j].teamValue;
-						homeTeamMascot = yFile[k][j].teamMascot;
-						homeTeamImage = yFile[k][j].teamImage;
-					}
-				}
-			}
-			// set away team values 
-			var away = xFile[key][i].awayTeam;
-			for (var k in yFile) {
-				for (var j = 0; j < yFile[k].length; j++) {
-					if(away == yFile[k][j].teamValue) {
-						awayTeam = yFile[k][j].team;
-						awayTeamVal = yFile[k][j].teamValue;
-						awayTeamMascot = yFile[k][j].teamMascot;
-						awayTeamImage = yFile[k][j].teamImage;
-					}
-				}
-			}
-			var spread = xFile[key][i].spread;
-			var fav = xFile[key][i].favorite;
-			var active = xFile[key][i].active;
-			var activeBg = "light";
-			var homeSide = "-";
-			var awaySide = "+";
-			var channel = xFile[key][i].channel;
-			var date = new Date(xFile[key][i].gameTime);
-			var badge = document.createElement('div');
-			if (fav != home) {
-			  homeSide = "+";
-			  awaySide = "-";		
-			}
-			if (spread == 0) {
-			  homeSide = "";
-			  awaySide = "";
-			  spread = "PK";
-			}
-			if (active) {
-				arrayActive.push(gameId);
-				activeBg = "style='background-color: #e3f2fd;'";
-			}
-			//badge.className = 'games-layout';
-			//badge.id = 'games-layout';
-			var header = `<div class='p-3 border bg-${activeBg}' ${activeBg}><span class='header'><h5 class='pb-2 lh-1' style='margin-bottom: 0px;'>${awayTeam} vs ${homeTeam} (${homeSide}${spread}) </h5>`;
-			var gameInfo = '<small class=\'w-100\'> '+ channel + " • " + date.toLocaleString([], {weekday: 'long', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'}).replace(',',' •') + '</small>';
-			var fin = '';
-
-			if (xFile[key][i].cover) {
-				if (xFile[key][i].cover == home) {
-					var awayScore = `<span class="teamlistfinal"><span class="p-2"><img src="https://b.fssta.com/uploads/application/college/team-logos/${awayTeamImage}.vresize.50.50.medium.2.png" width="25" height="25"><span class="p-1" id="away">${awayTeam} ${awayTeamMascot} - ${xFile[key][i].awayScore} </span></span> </span>`
-					var homeScore = `<span class="teamlistfinal"><span class="p-2"><img src="https://b.fssta.com/uploads/application/college/team-logos/${homeTeamImage}.vresize.50.50.medium.2.png" width="25" height="25"><span class="p-1" id="home">${homeTeam} ${homeTeamMascot} - ${xFile[key][i].homeScore} 💰 </span></span> </span>`
-				}
-				else if (xFile[key][i].cover == away) {
-					var awayScore = `<span class="teamlistfinal"><span class="p-2"><img src="https://b.fssta.com/uploads/application/college/team-logos/${awayTeamImage}.vresize.50.50.medium.2.png" width="25" height="25"><span class="p-1" id="away">${awayTeam} ${awayTeamMascot} - ${xFile[key][i].awayScore} 💰 </span></span> </span>`
-					var homeScore = `<span class="teamlistfinal"><span class="p-2"><img src="https://b.fssta.com/uploads/application/college/team-logos/${homeTeamImage}.vresize.50.50.medium.2.png" width="25" height="25"><span class="p-1" id="home">${homeTeam} ${homeTeamMascot} - ${xFile[key][i].homeScore}</span></span> </span>`
-				}
-				select = awayScore + homeScore + "</span>";
-				gameInfo = '';
-				if (xFile[key][i].overtime) {
-					if (xFile[key][i].numberOfOT > 1) {
-						numOT = xFile[key][i].numberOfOT.toString();
-						numOT = `(${numOT})`;
-					}
-					fin = `<sub><b>FINAL / OT ${numOT}</b></sub>`;
-				}
-				else {
-					fin = '<sub><b>FINAL</b></sub>';
-				}
-				var yourPick = document.createElement('div');
-				yourPick.id = 'your-pick-game' + gameId;
-				yourPick.innerHTML = '';
-			}
-			else if (spread == "-") {
-				var select = '';	
-			}
-			else {
-				var select = '<select class=\'teamlist form-select form-select-sm\' style=\'width:auto;\' id=\'game' + gameId + '\' onchange=\"assignPointsByTeam(' + gameId +');\"><option value = \"\"> -- Select Team -- </option><option value=\"' + awayTeamVal + '\">' + awayTeam + ' ' + awaySide + spread + '</option><option value=\"' + homeTeamVal + '\">' + homeTeam + ' ' + homeSide + spread + '</option></select>'; 	
-			}
-			badge.innerHTML = '<form>' + header + gameInfo + select + fin + '</form></div>';
-			document.getElementById(key).appendChild(badge);
-			//document.getElementsByClassName(badge.id)[i].appendChild(yourPick);
-		}
-		
-		// set first game of the day
-		var firstSet = [];
-		var first = 0;
-
-		for(var i = 1; i < 11; i++) {
-			if(typeof xFile[key][i-1].cover == 'undefined'){
-				if (first == 0)
-					first = i;
-			}
-			if (!arrayActive.includes(i)) {	
-				var element = document.getElementById('game-' + i);
-				element.classList.add("bg-light");
-			}
-		}
-		arrayActive.forEach(myFunction);
-		var date2 = new Date(xFile[key][first - 1].gameTime);
-		active = xFile[key][first - 1].active;
-	}
   
-	// if current time is after start time of first game, lock
-	if (active !== undefined && !active) {
-		document.getElementById("saver").innerHTML = `<button type="submit" id="savePicks" disabled class='btn btn-primary'>Lines Not Locked</button>`;
-	}
-	else {
+  var choices = [{teamAbb: "", fullTeam: "", game: "", spread : "", pts : "50"}, {teamAbb: "", fullTeam: "", game: "", spread : "", pts : "40"}, {teamAbb: "", fullTeam: "", game: "", spread : "", pts : "30"}, {teamAbb: "", fullTeam: "", game: "", spread : "", pts : "20"}, {teamAbb: "", fullTeam: "", game: "", spread : "", pts : "10"}];
+  
+  var gameId = "";
+  var home = "";
+  var homeTeam = "";
+  var homeTeamVal = "";
+  var homeTeamMascot = "";
+  var homeTeamImage = "";
+  var away = "";
+  var awayTeam = "";
+  var awayTeamVal = "";
+  var spread = "";
+  var fav ="";
+  var homeSide = "";
+  var awaySide = "";
+  var required = "";
+  var num = 0;
+  
+	var xFile, yFile;
+  
+	var requestX = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/games/week" + week + ".json", function(json){
+		xFile = json;
+	});
+  
+	var requestY = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/teams.json", function(json){
+		yFile = json;
+	});
+  
+	var date1 = new Date();
+  
+  $.when(requestX, requestY).then(function(){
+	  for (var key in xFile) {
+		  for (var i = 0; i < xFile[key].length; i++) {
+			  var gameId = xFile[key][i].gameId;
+			  // set home team values
+			  var home = xFile[key][i].homeTeam;
+			  for (var k in yFile) {
+				  for (var j = 0; j < yFile[k].length; j++) {
+					  if(home == yFile[k][j].teamValue) {
+						  homeTeam = yFile[k][j].team;
+						  homeTeamVal = yFile[k][j].teamValue;
+						  homeTeamMascot = yFile[k][j].teamMascot;
+						  homeTeamImage = yFile[k][j].teamImage;
+					  }
+				  }
+			  }
+			  // set away team values 
+			  var away = xFile[key][i].awayTeam;
+			  for (var k in yFile) {
+				  for (var j = 0; j < yFile[k].length; j++) {
+					  if(away == yFile[k][j].teamValue) {
+						  awayTeam = yFile[k][j].team;
+						  awayTeamVal = yFile[k][j].teamValue;
+						  awayTeamMascot = yFile[k][j].teamMascot;
+						  awayTeamImage = yFile[k][j].teamImage;
+					  }
+				  }
+			  }
+			  var spread = xFile[key][i].spread;
+			  var fav = xFile[key][i].favorite;
+			  var homeSide = "-";
+			  var awaySide = "+";
+			  var channel = xFile[key][i].channel;
+			  var date = new Date(xFile[key][i].gameTime);
+			  var date2 = new Date(xFile[key][0].gameTime);
+			  var required = xFile[key][i].required;
+			  var badge = document.createElement('div');
+			  var col = document.createElement('div');
+			  var number = num + 1; 
+			  col.className = 'col';
+			  col.id = 'col' + number;
+			  var isRequired = false;
+			  var requiredField = "";
+
+			  if (fav != home) {
+				  homeSide = "+";
+				  awaySide = "-";		
+			  }
+			  if (spread == 0) {
+				  homeSide = "";
+				  awaySide = "";
+				  spread = "PK";
+			  }
+			  if (required) {
+				  requiredField = 'bg-warning bg-gradient';
+				  isRequired = true;
+			  }
+			  else {
+				  requiredField = 'bg-light';		
+			  }
+			  var header = '<div class=\'p-3 border '+ requiredField +'\'><span class=\'header\'><h5 class=\'pt-2 lh-1\' style=\'margin-bottom: 0px;\'>' + awayTeam + ' vs ' + homeTeam + ' (' + homeSide + spread + ') </h5>';
+			  var gameInfo = '<small class=\'w-100\'> '+ channel + " · " + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }).replace(/^0+/, '') + '</small>';
+			  var select = '<select class=\'teamlist form-select form-select-sm\' style=\'width:auto;\' id=\'game' + gameId + '\' onchange=\"showPointTotals(\'point_totals_game_' + gameId + '\', this);\"><option value = \"\"> -- Select Team -- </option><option value=\"' + awayTeamVal + '\">' + awayTeam + ' ' + awaySide + spread + '</option><option value=\"' + homeTeamVal + '\">' + homeTeam + ' ' + homeSide + spread + '</option></select>';
+			  var display = '<div class=\'pt-3 lh-1\' id=\"point_totals_game_' + gameId + '\" style=\"display:none;\">';
+			  var numbers = [5, 4, 3, 2, 1];
+			  var radios = [];
+			  for (var j = 0; j < numbers.length; j++) {
+				  if (required) {
+					  var radio = '<label class="radio-inline font-weight-bold px-3"><input class="form-check-input" type=\"radio\" onchange=\"assignPointsByTeam(this.value, ' + gameId +', '+ isRequired +');\" name=\"pick_worth\" value=' + numbers[j] + '0 required> <br>' + numbers[j] + '0</label>'
+				  } 
+				  else {
+					  var radio = '<label class="radio-inline font-weight-bold px-3"><input class="form-check-input" type=\"radio\" onchange=\"assignPointsByTeam(this.value, ' + gameId +');\" name=\"pick_worth\" value=' + numbers[j] + '0> <br>' + numbers[j] + '0</label>'
+				  }
+				  radios.push(radio);
+			  }
+			  badge.innerHTML = '<form>' + header + gameInfo + select + display + radios.join(' ') + '</form></div>';		
+			  document.getElementById(key).appendChild(col);
+			  document.getElementById("col" + number).appendChild(badge);
+			  num = number;
+		  }
+	  }
+	  
+	  	// if current time is after start time of first game, lock
 		if (date1.getTime() > date2.getTime()) {
 			document.getElementById("saver").innerHTML = `<button type="submit" disabled id="savePicks" class='btn btn-primary'>Picks Locked</button>`;
 		}
 		else {
-			document.getElementById("saver").innerHTML = `<button type="submit" id="savePicks" class='btn btn-primary'>Save My Picks</button>`;
-		}
-	}/**/
-});
-  
-var request;
-
-function assignPointsByTeam(id) {
-	var pick = document.getElementById("game" + id);
-	var userPick = pick.options[pick.selectedIndex].value;
-	var fullTeamName = pick.options[pick.selectedIndex].text;
-	fullTeamSpread = fullTeamName.replace(/[^\d+.-]/g, '');
-	game.team = userPick;
-	document.getElementById("seasongame" + id).value = game.team;
-	document.getElementById("label-choice-seasongame" + id).innerHTML = `<label for="${id}" class="choice pb-1">${game.team} ${fullTeamSpread}</label>`;
-	getTeamInfo(userPick);
-
-	request.success(function(response){
-		game.spread = attempt.thisTeamImg;
-		document.getElementById("image" + id).innerHTML = `<img src="https://b.fssta.com/uploads/application/college/team-logos/${game.spread}.vresize.200.200.medium.2.png">`;
-	});
-	document.getElementById("seasongame" + id).value = game.team;
-}
-
-function setTeam(element) {
-	game.team = element.value;
-}
-
-function getTeamInfo(teamId) {
-	request = $.getJSON("https://codyphillips5.github.io/cbbpicks/json/teams.json", function(team) {
-		for (var key in team) {
-			for (var i = 0; i < team[key].length; i++) {
-				if(team[key][i].teamValue == teamId){
-					attempt.thisTeam = team[key][i].team;
-					attempt.thisTeamAbb = team[key][i].teamValue;
-					attempt.thisTeamImg = team[key][i].teamImage;
+			if (date1.getDay() < 4) {
+				document.getElementById("saver").innerHTML = `<button type="submit" disabled id="savePicks" class='btn btn-primary'>Picks Open Thursday</button>`;	
+			}
+			else {
+				// if day is Thursday before noon, picks open soon
+				if (date1.getDay() == 4 && date1.getHours() < 12) {
+					document.getElementById("saver").innerHTML = `<button type="submit" disabled id="savePicks" class='btn btn-primary'>Picks Open Soon</button>`;
+				}
+				// else picks are open
+				else {
+					document.getElementById("saver").innerHTML = `<button type="submit" id="savePicks" class='btn btn-primary'>Save My Picks</button>`;
 				}
 			}
 		}
-	});	
-}
-
-function myFunction(arrayValues) {	
-	const note = document.querySelector('#game-' + arrayValues);
-	note.style.backgroundColor = '#e3f2fd';
-	
-	document.getElementById("seasongame"+arrayValues).required = true;
-}
+  });
+  
+  var request;
+  
+  function assignPointsByTeam(pts, id, req) {
+	  var pick = document.getElementById("game" + id);
+	  var userPick = pick.options[pick.selectedIndex].value;
+	  var fullTeamName = pick.options[pick.selectedIndex].text;
+	  fullTeamSpread = fullTeamName.replace(/[^\d+.-]/g, '');
+	  game.team = userPick;
+	  game.spread = attempt.thisTeamImg;
+	  if (req == true) {
+		document.getElementById(pts + "-point-maker").className = "p-3 border bg-warning bg-gradient gotw";
+	  }
+	  document.getElementById(pts).value = game.team;
+	  document.getElementById("label-choice-" + pts).innerHTML = `<label for="${pts}" class="choice">${game.team} ${fullTeamSpread}</label>`;
+	  getTeamInfo(userPick);
+	  request.success(function(response){
+		  for (i = 0; i < choices.length; i++) {
+			  // only allow a team to be chosen once
+			  if (game.team == choices[i].teamAbb) {
+				  choices[i].teamAbb = "";
+				  choices[i].fullTeam = "";
+				  document.getElementById(choices[i].pts + "-point-maker").className = "p-3 border bg-light";
+				  document.getElementById(choices[i].pts).value = "";
+				  document.getElementById("label-choice-" + choices[i].pts).innerHTML = `<label for="${choices[i].pts}" class="choice"></label>`;
+				  document.getElementById("image" + choices[i].pts).innerHTML = ``;
+			  }
+			  if (pts == choices[i].pts) {
+				  if(choices[i].teamAbb != "") {
+					  console.log("game.game = " + game.game);
+					  if (req == true) {
+						document.getElementById(pts + "-point-maker").className = "p-3 border bg-warning bg-gradient gotw";
+					}
+					else {
+						document.getElementById(pts + "-point-maker").className = "p-3 border bg-light";
+					}
+						  if(choices[i].game != game.game) {
+						  var inputs = document.getElementById("point_totals_game_" + choices[i].game).getElementsByTagName("input");
+						  for (j = 0; j < inputs.length; j++) {
+							  inputs[j].checked = false;
+						  }
+					  }
+				  }
+				  // The request is done, and we can do something else
+				  choices[i].teamAbb = userPick;
+				  choices[i].fullTeam = fullTeamName;
+				  choices[i].game = id;
+				  choices[i].spread = attempt.thisTeamImg;
+				  document.getElementById("image" + pts).innerHTML = `<img class="pt-2" src="https://b.fssta.com/uploads/application/college/team-logos/${choices[i].spread}.vresize.200.200.medium.0.png">`;
+				  game.game = id;
+			  }
+		  }
+	  });
+	  document.getElementById(pts).value = game.team;
+  }
+  
+  function showPointTotals(divId, element){
+	  document.getElementById(divId).style.display = element.value != "" ? 'block' : 'none';
+	  var gm = divId.substring(divId.lastIndexOf("_") + 1);
+	  console.log(choices);
+	  for (i = 0; i < choices.length; i++) {
+		  // only allow a team to be chosen once
+		  if (choices[i].game == gm){ 
+			  if (element.value != choices[i].teamAbb) {
+				  if (choices[i].pts) {
+					  console.log("goner");
+					  document.getElementById(choices[i].pts).value = "";
+					  document.getElementById(choices[i].pts + "-point-maker").className = "p-3 border bg-light";
+					  document.getElementById("label-choice-" + choices[i].pts).innerHTML = `<label for="${choices[i].pts}" class="choice"></label>`;
+					  document.getElementById("image" + choices[i].pts).innerHTML = ``;
+					  choices[i].teamAbb = "";
+					  choices[i].fullTeam = "";
+					  choices[i].game = "";
+					  choices[i].spread = "";
+				  }
+			  }
+		  }
+	  }
+  
+	  var inputs = document.getElementById("point_totals_game_" + gm).getElementsByTagName("input");
+	  for (j = 0; j < inputs.length; j++) {
+		  inputs[j].checked = false;
+	  }
+	  game.team = element.value;
+	  game.game = gm;
+  }
+  
+  function setTeam(element) {
+	  game.team = element.value;
+  }
+  
+  // save picks
+  const savePicks = document.querySelector('#save_picks');
+  if(savePicks) {
+	  savePicks.addEventListener('submit', (e) => {
+		  try {
+			  e.preventDefault();
+			  const fifty = document.getElementById('50').value;
+			  const forty = document.getElementById('40').value;
+			  const thirty = document.getElementById('30').value;
+			  const twenty = document.getElementById('20').value;
+			  const ten = document.getElementById('10').value;
+			  console.log(fifty);
+		  }
+		  catch (err) {
+			  console.log(err);
+			  console.log(err.message);
+		  }
+	  })
+  }
+  
+  function getTeamInfo(teamId) {
+	  request = $.getJSON("https://codyphillips5.github.io/cfbpicks/json/teams.json", function(team) {
+	  for (var key in team) {
+		  for (var i = 0; i < team[key].length; i++) {
+			  if(team[key][i].teamValue == teamId){
+				  attempt.thisTeam = team[key][i].team;
+				  attempt.thisTeamAbb = team[key][i].teamValue;
+				  attempt.thisTeamImg = team[key][i].teamImage;
+			  }
+		  }
+	  }
+  });	
+  }

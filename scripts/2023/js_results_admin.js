5e5e20266aca0a5fb9d7d2a63bd0888e9d765652
Cody Phillips <codyphill5@gmail.com>
2023-10-19 01:45:08 -0400

    crompton

--- scripts/2023/js_results_admin.js
+++ scripts/2023/js_results_admin.js
@@ -14,7 +14,7 @@ var coversNum = [];
 var coversTeam = [];
 var userPickTeams = [];
 var coversArr = [""];
-var weekNum = 7;
+var weekNum = 8;
 var isCorrect;
 var isMe;
 var date1 = new Date();
@@ -31,7 +31,7 @@ var resultsList = [];
 var badge = document.createElement('div');
 badge.className = 'results';
 
-var select = `<button onclick='getResultsByWeek(7);return false;' id='desktop_buttons' class='btn btn-secondary'>Week 7</button>`;
+var select = `<button onclick='getResultsByWeek(8);return false;' id='desktop_buttons' class='btn btn-secondary'>Week 8</button>`;
 	 
 badge.innerHTML = '<form>' + select + '</form>';		
 document.getElementById("weeks").appendChild(badge);

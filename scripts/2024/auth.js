86aab23a216bda4ca15ec10c2cd54870a9dd1532
codyp <cody.phillips@fox.com>
2024-09-16 11:17:00 -0400

    cedric tillman

--- scripts/2024/auth.js
+++ scripts/2024/auth.js
@@ -19,7 +19,7 @@ auth.onAuthStateChanged(user => {
 })
  
 // also update line 115
-var weekNum = 3;
+var weekNum = 4;
 var fn;
 var requiredSelected = true;
 // create new guide
@@ -118,7 +118,7 @@ if(saveResults) {
 					// update the number every week 
 					const data = {
 						user: usercheck,
-						week3: {
+						week4: {
 							id: weekcheck,
 							points: pointscheck,
 							top: topcheck

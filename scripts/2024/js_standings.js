cb45856459e7397c6f2852e8ddf4cfcb59088e5a
codyp <cody.phillips@fox.com>
2024-09-29 12:05:28 -0400

    bru mccoy

--- scripts/2024/js_standings.js
+++ scripts/2024/js_standings.js
@@ -9,7 +9,7 @@ var myUsername = "";
 var myPoints = "";
 var itsme = false;
 
-var weekNum = 4;
+var weekNum = 5;
 var userWeekTop = false;
 
 var users = db.collection("Users").get().then((querySnapshot) => {

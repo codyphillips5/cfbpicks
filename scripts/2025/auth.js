4d5009ad9be559f64d0f4c899fc81930cca6efad
unknown <cody.phillips@fox.com>
2025-10-27 10:56:54 -0400

    ainge

--- scripts/2025/auth.js
+++ scripts/2025/auth.js
@@ -19,7 +19,7 @@ auth.onAuthStateChanged(user => {
 })
  
 // also update line 121
-var weekNum = 9;
+var weekNum = 10;
 var fn;
 var requiredSelected = true;
 // create new guide
@@ -118,7 +118,7 @@ if(saveResults) {
 					// update the number every week 
 					const data = {
 						user: usercheck,
-						week9: {
+						week10: {
 							id: weekcheck,
 							points: pointscheck,
 							top: topcheck

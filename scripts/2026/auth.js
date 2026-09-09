4ab8113031e7e4f9338a5c0e784a7d2a320e67de
Cody <codyphill5@gmail.com>
2026-09-09 13:14:36 -0400

    shy tut

--- scripts/2026/auth.js
+++ scripts/2026/auth.js
@@ -19,7 +19,7 @@ auth.onAuthStateChanged(user => {
 })
  
 // also update line 121
-var weekNum = 1;
+var weekNum = 2;
 var fn;
 var requiredSelected = true;
 // create new guide
@@ -118,7 +118,7 @@ if(saveResults) {
 					// update the number every week 
 					const data = {
 						user: usercheck,
-						week1: {
+						week2: {
 							id: weekcheck,
 							points: pointscheck,
 							top: topcheck
